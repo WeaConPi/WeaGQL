1c5e5247a390af8760123ef82d4f09e83b17c48b
Vojtěch Baránek <vojtabaranek@gmail.com>
2017-09-17 21:55:15 +0200

    remove deprecated connection

--- mongoose/connection.ts
+++ mongoose/connection.ts
@@ -1,5 +1,15 @@
 import mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.mongoURL);
+var promise = mongoose.createConnection(process.env.mongoURL, {
+    useMongoClient: true,
+    /* other options */
+});
+promise.then(function(db) {
+    console.log('Mongo connected')
+});
+promise.catch(function (e){
+    console.log('Connection error :',e )
+})
+// mongoose.connect(process.env.mongoURL);
 export { mongoose };
