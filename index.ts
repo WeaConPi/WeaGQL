883b2c08f21c0dc80671603d3693bd8523903e1f
Vojtěch Baránek <vojtabaranek@gmail.com>
2017-10-16 21:27:45 +0200

    remove comments

--- index.ts
+++ index.ts
@@ -8,11 +8,7 @@ import schema from './schema/schema';
 import { graphiqlConnect } from 'apollo-server-express';
 import cors = require('cors');
 import REST from './rest-api/index';
-import { HourM } from "./schema/Hour";
-import { DayM } from "./schema/Day";
 const app = express();
-let ObjectId = require('mongoose').Types.ObjectId;
-
 const PORT = process.env.PORT || 6004;
 app.use(cors());
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: schema }));
@@ -27,20 +23,6 @@ app.get('/api/hello2', async (req, res) => {
     res.json({ hello: 'world - 2' });
 });
 app.use('/api',REST )
-// app.get('/api/hours/:dayId', async (req, res) => {
-//     const hours = await HourM.find({ dayId: new ObjectId(req.params.dayId) });
-//     res.json(hours);
-// });
-// app.get('/api/day/building/:buildingId', async (req, res) => {
-//     console.log('dafaaaq')
-//
-//     const day = await DayM.findOne({
-//         buildingId: req.params.buildingId,
-//         date: new Date(req.query.date),
-//     });
-//     res.json(day);
-// });
-
 
 app.listen(PORT, () => {
   console.log(`Start is up and running on localhost:${PORT}`);
