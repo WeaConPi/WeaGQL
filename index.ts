96b0935845904373b565543d7d6bf38b4f8d308b
Vojtěch Baránek <vojtabaranek@gmail.com>
2017-10-16 20:51:26 +0200

    hellol world

--- index.ts
+++ index.ts
@@ -1,24 +1,27 @@
 /**
  * Created by Farmas on 04.09.2017.
  */
-import * as express from "express";
+import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { graphqlExpress } from 'apollo-server-express';
-import schema from './schema/schema'
+import schema from './schema/schema';
 import { graphiqlConnect } from 'apollo-server-express';
-import cors = require('cors')
-import { initializeREST } from "./rest-api/index";
+import cors = require('cors');
+import { initializeREST } from './rest-api/index';
 const app = express();
 
-const PORT = (process.env.PORT || 6004);
-app.use(cors())
-app.use('/graphql', bodyParser.json(), graphqlExpress({schema: schema}));
+const PORT = process.env.PORT || 6004;
+app.use(cors());
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: schema }));
 
-app.use('/graphiql', graphiqlConnect({
+app.use(
+  '/graphiql',
+  graphiqlConnect({
     endpointURL: '/graphql',
-}));
+  }),
+);
 
-initializeREST(app)
+initializeREST(app);
 app.listen(PORT, () => {
-    console.log(`Start is up and running on localhost:${PORT}`)
+  console.log(`Start is up and running on localhost:${PORT}`);
 });
--- rest-api/index.ts
+++ rest-api/index.ts
@@ -3,13 +3,14 @@ import { HourM } from '../schema/Hour';
 let ObjectId = require('mongoose').Types.ObjectId;
 
 export const initializeREST = app => {
+  app.get('/api/hello', async (req, res) => {
+    res.json({ hello: 'world' });
+  });
   app.get('/api/hours/:dayId', async (req, res) => {
     const hours = await HourM.find({ dayId: new ObjectId(req.params.dayId) });
     res.json(hours);
   });
   app.get('/api/day/building/:buildingId', async (req, res) => {
-    console.log(req.query.date);
-    console.log(req.params.buildingId);
     const day = await DayM.findOne({
       buildingId: req.params.buildingId,
       date: new Date(req.query.date),
